9b50f78a68576f856829eedc676afd1a7523730d
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-12-26 13:29:20 +0100

    Add clean tasks

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
-require 'bundler/gem_tasks'
+require 'rake/clean'
 require 'rake/testtask'
+require 'bundler/gem_tasks'
+
+STDOUT.sync = STDERR.sync = true
+
+CLOBBER << 'prj'
 
 Rake::TestTask.new(:test) do |t|
   t.ruby_opts = %w[-w]
