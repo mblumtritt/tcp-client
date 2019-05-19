f1a0b5ad382b8e630e5fa4c0cb5f04f099db65af
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2019-05-19 10:25:41 +0200

    Lazy #to_s for Address

--- lib/tcp-client/address.rb
+++ lib/tcp-client/address.rb
@@ -4,7 +4,7 @@ require 'socket'
 
 class TCPClient
   class Address
-    attr_reader :to_s, :hostname, :addrinfo
+    attr_reader :hostname, :addrinfo
 
     def initialize(addr)
       case addr
@@ -20,17 +20,20 @@ class TCPClient
       @addrinfo.freeze
     end
 
+    def to_s
+      return "[#{@hostname}]:#{@addrinfo.ip_port}" if @hostname.index(':') # IP6
+      "#{@hostname}:#{@addrinfo.ip_port}"
+    end
+
     private
 
     def init_from_selfclass(address)
-      @to_s = address.to_s
       @hostname = address.hostname
       @addrinfo = address.addrinfo
     end
 
     def init_from_addrinfo(addrinfo)
-      @hostname, port = addrinfo.getnameinfo(Socket::NI_NUMERICSERV)
-      @to_s = "#{@hostname}:#{port}"
+      @hostname, _port = addrinfo.getnameinfo(Socket::NI_NUMERICSERV)
       @addrinfo = addrinfo
     end
 
@@ -38,7 +41,6 @@ class TCPClient
       @hostname, port = from_string(str.to_s)
       return init_from_addrinfo(Addrinfo.tcp(nil, port)) unless @hostname
       @addrinfo = Addrinfo.tcp(@hostname, port)
-      @to_s = as_str(@hostname, port)
     end
 
     def from_string(str)
@@ -46,11 +48,6 @@ class TCPClient
       name = str[0, idx]
       name = name[1, name.size - 2] if name[0] == '[' && name[-1] == ']'
       [name, str[idx + 1, str.size - idx].to_i]
-    end
-
-    def as_str(hostname, port)
-      return "[#{hostname}]:#{port}" if hostname.index(':') # IP6
-      "#{hostname}:#{port}"
     end
   end
 end
