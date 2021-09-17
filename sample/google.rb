c3f5edb6efab71fcb383d259b27357a3c7aff2c1
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2021-09-17 08:20:16 +0200

    Improve samples

--- sample/google.rb
+++ sample/google.rb
@@ -2,21 +2,20 @@
 
 require_relative '../lib/tcp-client'
 
-TCPClient.configure(
-  connect_timeout: 0.5, # seconds to connect the server
-  write_timeout: 0.25, # seconds to write a single data junk
-  read_timeout: 0.5 # seconds to read some bytes
-)
+# global configuration.
+# - 0.5 seconds to connect the server
+# - 0.25 seconds to write a single data junk
+# - 0.25 seconds to read some bytes
+TCPClient.configure do |cfg|
+  cfg.connect_timeout = 0.5
+  cfg.write_timeout = 0.25
+  cfg.read_timeout = 0.25
+end
 
-# the following sequence is not allowed to last longer than 1.25 seconds:
-#   0.5 seconds to connect
-# + 0.25 seconds to write data
-# + 0.5 seconds to read a response
-
+# request to Google:
+# - send a simple HTTP get request
+# - read 12 byte: "HTTP/1.1 " + 3 byte HTTP status code
 TCPClient.open('www.google.com:80') do |client|
-  # simple HTTP get request
-  pp client.write("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-
-  # read "HTTP/1.1 " + 3 byte HTTP status code
-  pp client.read(12)
+  p client.write("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+  p client.read(12)
 end
--- sample/google_ssl.rb
+++ sample/google_ssl.rb
@@ -2,18 +2,23 @@
 
 require_relative '../lib/tcp-client'
 
-TCPClient.configure do |cfg|
-  cfg.connect_timeout = 1 # limit connect time the server to 1 second
-  cfg.ssl_params = { ssl_version: :TLSv1_2 } # use TLS 1.2
-end
+# create a configuration.
+# - use TLS 1.2
+# - don't use internal buffering
+cfg =
+  TCPClient::Configuration.create(
+    buffered: false,
+    ssl_params: {
+      ssl_version: :TLSv1_2
+    }
+  )
 
-TCPClient.open('www.google.com:443') do |client|
-  # next sequence should not last longer than 0.5 seconds
-  client.with_deadline(0.5) do
-    # simple HTTP get request
-    pp client.write("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-
-    # read "HTTP/1.1 " + 3 byte HTTP status code
-    pp client.read(12)
-  end
+# request to Google:
+# - limit all interactions to 0.5 seconds
+# - use the Configuration cfg
+# - send a simple HTTP get request
+# - read 12 byte: "HTTP/1.1 " + 3 byte HTTP status code
+TCPClient.with_deadline(0.5, 'www.google.com:443', cfg) do |client|
+  p client.write("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+  p client.read(12)
 end
